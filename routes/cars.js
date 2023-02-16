d0a4fc844fe551830a0f9ce1262cbede2c41dc47
Pin Chen <pamyc1876@gmail.com>
2023-02-15 20:37:33 -0500

    Implement the update function

--- routes/cars.js
+++ routes/cars.js
@@ -3,5 +3,6 @@ const carsCtrl = require('../controllers/cars.js')
 
 router.post('/', carsCtrl.create)
 router.get('/', carsCtrl.index)
+router.put('/:id', carsCtrl.update)
 
 module.exports = router
