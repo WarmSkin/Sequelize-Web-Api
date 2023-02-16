a03d74dd93124359181d32a4919f274afd4c7077
Pin Chen <pamyc1876@gmail.com>
2023-02-15 20:33:33 -0500

    Implement the index function.

--- controllers/cars.js
+++ controllers/cars.js
@@ -10,6 +10,16 @@ const create = async (req, res) => {
   }
 }
 
+const index = async (req, res) => {
+  try {
+    const cars = await Car.findAll()
+    res.status(200).json(cars)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
 module.exports = {
   create,
+  index,
 }
--- routes/cars.js
+++ routes/cars.js
@@ -2,5 +2,6 @@ const router = require('express').Router()
 const carsCtrl = require('../controllers/cars.js')
 
 router.post('/', carsCtrl.create)
+router.get('/', carsCtrl.index)
 
 module.exports = router
