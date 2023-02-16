d5609ad2444d748dc0e503a3d9063dff44314f60
Pin Chen <pamyc1876@gmail.com>
2023-02-15 20:19:20 -0500

    Implement a create function for car.

--- controllers/cars.js
+++ controllers/cars.js
@@ -1,6 +1,15 @@
 const { Car } = require("../models")
 
 
-module.exports = {
+const create = async (req, res) => {
+  try {
+    const car = await Car.create(req.body)
+    res.status(200).json(car)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
 
+module.exports = {
+  create,
 }
